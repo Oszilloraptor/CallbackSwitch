34ce1b5ffab3fcbfa5c138ac5ea72f20204558fd
D. S. Richard Richter <richter@vivaldi.net>
2020-11-08 19:47:56 +0100

    Implement CaseNotFoundException + Test

---
+++ tests/functional/Exceptions/CaseNotFoundExceptionTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace RiktaD\CallbackSwitch\Test\Functional\Exceptions;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use RiktaD\CallbackSwitch\Exceptions\CaseNotFoundException;
+
+class CaseNotFoundExceptionTest extends TestCase
+{
+
+    /**
+     * @testdox returns message "No matching case found in {file}:{line}" by default
+     */
+    public function testDefaultMessage(): void
+    {
+        $exception = new CaseNotFoundException();
+        $result = $exception->getMessage();
+        $expected = sprintf(
+            'No matching case found in %s:%d',
+            __FILE__,
+            __LINE__ - 5
+        );
+
+        self::assertEquals($expected, $result);
+    }
+
+    /**
+     * @testdox returns code 0 by default
+     */
+    public function testDefaultCode(): void
+    {
+        $exception = new CaseNotFoundException();
+        $result = $exception->getCode();
+        $expected = 0;
+
+        self::assertEquals($expected, $result);
+    }
+
+    /**
+     * @testdox returns custom message if provided as 1nd parameter
+     */
+    public function testPassesMessage(): void
+    {
+        $message = 'dummy message';
+        $exception = new CaseNotFoundException($message);
+        $result = $exception->getMessage();
+
+        self::assertEquals($message, $result);
+    }
+
+    /**
+     * @testdox returns custom error code if provided as 2nd parameter
+     */
+    public function testPassesCode(): void
+    {
+        $exception = new CaseNotFoundException("", 1337);
+        $result = $exception->getCode();
+        $expected = 1337;
+
+        self::assertEquals($expected, $result);
+    }
+
+    /**
+     * @testdox returns previous exception if provided as 3rd parameter
+     */
+    public function testPassesPrevious(): void
+    {
+        $previous = new Exception();
+        $exception = new CaseNotFoundException("", 0, $previous);
+        $result = $exception->getPrevious();
+
+
+        self::assertEquals($previous, $result);
+    }
+}
