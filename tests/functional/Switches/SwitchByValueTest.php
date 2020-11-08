1cbfe554bc508989617cb53a09610795ab991700
D. S. Richard Richter <richter@vivaldi.net>
2020-11-08 21:58:33 +0100

    Implement SwitchByValue

---
+++ tests/functional/Switches/SwitchByValueTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RiktaD\CallbackSwitch\Test\Functional\Switches;
+
+use Generator;
+use RiktaD\CallbackSwitch\Switches\SwitchByValue;
+use PHPUnit\Framework\TestCase;
+
+class SwitchByValueTest extends TestCase
+{
+
+    /**
+     * @param int|string $value value to pass
+     * @param string $expected expected result
+     * @dataProvider correctCaseDataProvider
+     */
+    public static function testCorrectCaseIsChosen($value, string $expected): void
+    {
+        $switch = new SwitchByValue([
+            'a' => static fn () => 'A',
+            'b' => static fn () => 'B',
+            SwitchByValue::DEFAULT => fn () => 'default'
+        ]);
+
+        $result = $switch($value);
+
+        self::assertEquals($expected, $result);
+    }
+
+    /**
+     * @return Generator<mixed>
+     */
+    public function correctCaseDataProvider(): Generator
+    {
+        yield [
+            'a',
+            'A'
+        ];
+
+        yield [
+            'b',
+            'B'
+        ];
+
+        yield [
+            'unknown',
+            'default'
+        ];
+    }
+}
